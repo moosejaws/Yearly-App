27b99cf72738d590abdfc027c0976376a5a8090a
moosejaws <53672982+moosejaws@users.noreply.github.com>
2022-04-25 20:52:03 -0700

    Update goalRouter.js

--- routes/goalRouter.js
+++ routes/goalRouter.js
@@ -73,29 +73,11 @@ goalRouter.delete("/:goalId", (req, res, next) => {
 })
 
 
-
-/*goalRouter.put("/:goalId", (req, res, next) => {
-  req.body.user = req.user._id
-  req.body.goalId = req.params.goalId
-  Goal.findOneAndUpdate(
-    ((err, updatedGoal) => {
-      if (err) {
-        res.status(500)
-        return next(err)
-      }
-      return res.status(201).send(updatedGoal)
-    }
-  ))
-})
-*/
-
-
 goalRouter.put("/isCompleted/:goalId", (req, res, next) => {
   Goal.findOneAndUpdate(
     {_id: req.params.goalId,
     user: req.user._id },
     { $set: { isCompleted: true } },
-// necessary?
     function (err, goal) {
     goal.isCompleted = !goal.isCompleted;
     goal.save( function (err, updatedGoal) {
@@ -107,46 +89,6 @@ goalRouter.put("/isCompleted/:goalId", (req, res, next) => {
     })
   })
 })
-/*Book.findOne({ _id: req.params.id }, function(err, book) {
-    book.sold = !book.sold;
-    book.save(function(err, updatedBook) {
-        ...
-    });
-}); */
 
-
-/* 
-issueRouter.put("/upvotes/:issueId", (req, res, next) => {
-  Issue.findOneAndUpdate(
-    { _id: req.params.issueId },
-    { $inc: { upVotes: 1 } },
-    { new: true },
-    (err, issue) => {
-      if (err) {
-        res.status(500)
-        return next(err)
-      }
-      return res.status(201).send(issue)
-    }
-  )
-})
-//Downvote
-issueRouter.put("/downvotes/:issueId", (req, res, next) => {
-  Issue.findOneAndUpdate(
-    { _id: req.params.issueId },
-    { $inc: { downVotes: -1 } },
-    { new: true },
-    (err, issue) => {
-      if (err) {
-        res.status(500)
-      }
-      return res.status(200).send(issue)
-    })
-})
-
-*/
-
-
-//is completed?? put request upvote/downvote type stuff
 
 module.exports = goalRouter
